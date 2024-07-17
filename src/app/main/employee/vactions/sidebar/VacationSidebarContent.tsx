d94dce51223d26534838e7e8efa7f77067126df9
Yu Jueun <jueunyu72@gmail.com>
2024-07-17 17:11:45 +0900

    feat: add right sidebar content

---
+++ src/app/main/employee/vactions/sidebar/VacationSidebarContent.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const VacationSidebarContent = () => {
+    return (
+        <div>
+            <h2>Sidebar Content</h2>
+            <p>This is the sidebar content for Vacation App.</p>
+        </div>
+    );
+}
