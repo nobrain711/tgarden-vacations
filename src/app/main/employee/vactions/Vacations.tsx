4ff34ee6eb9e576b206f05695c0a3d43d0b5dcdf
Yu Jueun <jueunyu72@gmail.com>
2024-07-17 17:11:45 +0900

    feat: add right sidebar content

--- src/app/main/employee/vactions/Vacations.tsx
+++ src/app/main/employee/vactions/Vacations.tsx
@@ -1,49 +1,54 @@
-import FusePageSimple from "@fuse/core/FusePageSimple";
-import { styled } from "@mui/styles";
-import { memo, useState } from "react";
-import Button from "@mui/material/Button";
-import { VacationsTabs } from "./tabs/VacationsTabs";
+import FusePageSimple from '@fuse/core/FusePageSimple';
+import { styled } from '@mui/styles';
+import { memo, useState } from 'react';
+import Button from '@mui/material/Button';
+import { VacationsTabs } from './tabs/VacationsTabs';
+import { VacationSidebarContent } from './sidebar/VacationSidebarContent';
 
 const Root = styled(FusePageSimple)(({ theme }) => ({
-  "& .FusePageSimple-header": {
-    backgroundColor: "#FFF",
-    borderBottomWidth: 1,
-    borderStyle: "solid",
-    borderColor: theme.palette.divider,
-  },
-  "& .FusePageSimple-content": {
-    backgroundColor: "#FFF",
-  },
-  "& .FusePageSimple-sidebarHeader": {},
-  "& .FusePageSimple-sidebarContent": {},
+	'& .FusePageSimple-header': {
+		backgroundColor: '#FFF',
+		borderBottomWidth: 1,
+		borderStyle: 'solid',
+		borderColor: theme.palette.divider
+	},
+	'& .FusePageSimple-content': {
+		backgroundColor: '#FFF'
+	},
+	'& .FusePageSimple-sidebarHeader': {},
+	'& .FusePageSimple-sidebarContent': {}
 }));
-const Vacations = () => {
-  const [rightSideBarOpen, setRightSideBarOpen] = useState<boolean>(false);
 
-  const handlerRightSideBar = () => {
-    console.log(!rightSideBarOpen);
-    setRightSideBarOpen(!rightSideBarOpen);
-  };
+function Vacations() {
+	const [rightSideBarOpen, setRightSideBarOpen] = useState<boolean>(false);
 
-  return (
-    <Root
-      header={
-        <div className="p-24">
-          <Button
-            variant="contained"
-            color="secondary"
-            className="font-900 w-400"
-            onClick={handlerRightSideBar}
-          >
-            休暇申請/本社出勤申請
-          </Button>
-        </div>
-      }
-      content={
-        <VacationsTabs />
-      }
-    />
-  );
-};
+	const handlerRightSideBar = () => {
+		console.log(!rightSideBarOpen);
+		setRightSideBarOpen(!rightSideBarOpen);
+	};
+
+	return (
+		<Root
+			header={
+				<div className="p-24">
+					<Button
+						variant="contained"
+						color="secondary"
+						className="font-900 w-400"
+						onClick={handlerRightSideBar}
+					>
+						休暇申請/本社出勤申請
+					</Button>
+				</div>
+			}
+			content={<VacationsTabs />}
+			rightSidebarContent={<VacationSidebarContent />}
+			rightSidebarOpen={rightSideBarOpen}
+			rightSidebarOnClose={() => setRightSideBarOpen(false)}
+			rightSidebarWidth={640}
+			rightSidebarVariant="temporary"
+		/>
+	);
+}
 
 export default memo(Vacations);
