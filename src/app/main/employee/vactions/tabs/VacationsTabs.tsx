13053d7277deabea9bb9e4dceb5a36a179b17baf
nobrain711 <donghwi@donghwi.>
2024-07-17 10:00:52 +0900

    feat: tabs functions

--- src/app/main/employee/vactions/tabs/VacationsTabs.tsx
+++ src/app/main/employee/vactions/tabs/VacationsTabs.tsx
@@ -1,11 +1,14 @@
 import React, { SyntheticEvent, useState } from "react";
 import Tabs from "@mui/material/Tabs";
 import { Box, Tab } from "@mui/material";
+import { VacationsCount } from "./count/VacationsCount";
+import { TabPanel } from "@mui/base";
+import { VacationsHistory } from "./history/VacationsHistory";
 
 export const VacationsTabs = () => {
   const [tabsNumber, setTabsNumber] = useState<number>(0);
 
-  const handlerTabsPage = (event: SyntheticEvent ,pageNumber: number) => {
+  const handlerTabsPage = (event: SyntheticEvent, pageNumber: number) => {
     setTabsNumber(pageNumber);
   };
 
@@ -22,6 +25,7 @@ export const VacationsTabs = () => {
           <Tab value={1} label="休暇履歴" />
         </Tabs>
       </Box>
+      {tabsNumber === 0 ? <VacationsCount />: <VacationsHistory />}
     </Box>
   );
 };
---
+++ src/app/main/employee/vactions/tabs/count/VacationsChart.tsx
@@ -0,0 +1,3 @@
+export const VacationsChart = () => {
+    return (<>Chart Aear</>)
+}
