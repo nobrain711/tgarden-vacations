915372a20e3a416e61d4fa39fc10a7c331acfed6
Yu Jueun <jueunyu72@gmail.com>
2024-07-22 11:49:05 +0900

    WIP *

--- src/app/main/employee/vactions/sidebar/VacationSidebarContent.tsx
+++ src/app/main/employee/vactions/sidebar/VacationSidebarContent.tsx
@@ -17,9 +17,14 @@ export function VacationSidebarContent() {
 
             <div className="p-10 -mt-70">
                 <p className="text-base text-gray-700 font-bold">日付*</p>
-                <div className="mt-2 border border-gray-300 rounded-md p-2">
-                    <div className="p-12">
-                    </div>
+                <div className="mt-2 flex items-center border border-gray-300 rounded-md p-2">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="h-16 w-16 text-gray-400 ml-10 mr-3">
+                        <rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect>
+                        <line x1="16" y1="2" x2="16" y2="6"></line>
+                        <line x1="8" y1="2" x2="8" y2="6"></line>
+                        <line x1="3" y1="10" x2="21" y2="10"></line>
+                    </svg>
+                    <input type="text" className="w-full outline-none px-4 py-2" placeholder="" />
                 </div>
                 <p className="text-xs font-bold text-gray-500 mt-2">YYYY-MM-DD</p>
             </div>
@@ -30,6 +35,9 @@ export function VacationSidebarContent() {
             
             <div className="p-10">
                 <p className="text-base text-gray-700 font-bold">区分*</p>
+                <div className="border border-gray-300 p-4 mt-4 h-32 rounded-md" id="selectionBox">
+                    <span class="pl-6">有給休暇（全休）</span>
+                </div>
             </div>
 
             {/* <div style={{ padding: '50px' }}>
@@ -38,6 +46,7 @@ export function VacationSidebarContent() {
 
             <div className="p-10">
                 <p className="text-base text-gray-700 font-bold">時間</p>
+                <p className="ml-6">8</p>
             </div>
 
             {/* <div style={{ padding: '20px 50px' }}>
@@ -55,7 +64,7 @@ export function VacationSidebarContent() {
                 />
             </div> */}
 
-            <div className="p-6 md:p-10">
+            <div className="p-6 md:p-10 mb-6">
                 <p className="text-base text-gray-700 font-bold">休暇事由 / 備考</p>
                 <textarea
                     className="w-full min-h-20 px-4 py-2 text-sm border border-gray-300 rounded"
@@ -79,11 +88,13 @@ export function VacationSidebarContent() {
                 申請
             </button> */}
 
-            <Button className="text-sm px-10 py-2 mt-4 mb-2 bg-gray-200 text-gray-600 font-bold rounded-full"
-                onClick={() => alert('Button clicked!')}
-            >
-                申請
-            </Button>
+            <div className="flex justify-center">
+                <Button className="text-base px-10 py-2 mt-4 mb-2 bg-gray-200 text-gray-600 font-bold rounded-full w-full mx-10"
+                        onClick={() => alert('Button clicked!')}
+                >
+                    申請
+                </Button>
+            </div>
 
         </div>
     );
