0536a115ff4288ead10f97207aa2bd0a92350be6
kms973 <newto9973@gmail.com>
2024-07-24 16:53:30 +0900

    counthistory2

--- src/app/main/employee/vactions/tabs/count/dialog/VacationsDialog.tsx
+++ src/app/main/employee/vactions/tabs/count/dialog/VacationsDialog.tsx
@@ -2,11 +2,6 @@ import {
   closeDialog,
   selectFuseDialogState,
 } from "@fuse/core/FuseDialog/fuseDialogSlice";
-<<<<<<< HEAD
-import { DialogActions, DialogContent, Button, Dialog, Typography } from "@mui/material";
-import ReactMarkdown from "react-markdown";
-import remarkGfm from "remark-gfm";
-=======
 import {
   DialogActions,
   DialogContent,
@@ -25,23 +20,11 @@ import {
   TableHead,
   TableRow,
 } from "@mui/material";
->>>>>>> 7f92832df5f39dea08d6c1632e774d119ea9dc81
 import { RootState } from "app/store/store";
 import { useAppDispatch } from "app/store/hooks";
 import { Fragment } from "react";
 import { useSelector } from "react-redux";
 
-<<<<<<< HEAD
-const fetchMarkdown = async () => {
-  const respone = await fetch(
-    "src/app/main/employee/vactions/tabs/count/dialog/VacationsInfo.md"
-  );
-  const text = await respone.text();
-  return text;
-};
-
-=======
->>>>>>> 7f92832df5f39dea08d6c1632e774d119ea9dc81
 export const VacationsDialog = () => {
   const dispatch = useAppDispatch();
   const isOpen = useSelector((state: RootState) =>
@@ -52,9 +35,6 @@ export const VacationsDialog = () => {
     <Fragment>
       <Dialog open={isOpen} scroll={"paper"} maxWidth="lg" fullWidth={true}>
         <DialogContent>
-<<<<<<< HEAD
-          <Typography>유급휴가제도개요 (NEW)</Typography>
-=======
           {" "}
           <h2 id="%EC%9C%A0%EA%B8%89%ED%9C%B4%EA%B0%80%EC%A0%9C%EB%8F%84%EA%B0%9C%EC%9A%94-new">
             유급휴가제도개요 (NEW)
@@ -162,7 +142,6 @@ export const VacationsDialog = () => {
           <h4 id="8-%EC%82%AC%EC%9A%A9%ED%95%98%EC%A7%80-%EC%95%8A%EC%9D%80-%EC%9C%A0%EA%B8%89%ED%9C%B4%EA%B0%80%EB%8A%94-%EB%8B%A4%EC%9D%8C%ED%95%B4%EB%A1%9C-%EC%9D%B4%EC%9B%94%EB%90%9C%EB%8B%A4-%EB%8B%A8-1%EB%85%84%EC%B9%98%EC%97%90-%ED%95%9C%ED%95%B4%EC%84%9C">
             8. 사용하지 않은 유급휴가는 다음해로 이월된다. 단, 1년치에 한해서
           </h4>
->>>>>>> 7f92832df5f39dea08d6c1632e774d119ea9dc81
         </DialogContent>
         <DialogActions>
           <Button
