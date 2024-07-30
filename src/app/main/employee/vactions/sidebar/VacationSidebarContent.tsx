af7a17e0abb00ed7e4efec3d8ad2e03b19e37335
Yu Jueun <jueunyu72@gmail.com>
2024-07-30 14:31:18 +0900

    feat asdf

--- src/app/main/employee/vactions/sidebar/VacationSidebarContent.tsx
+++ src/app/main/employee/vactions/sidebar/VacationSidebarContent.tsx
@@ -2,14 +2,15 @@ import React, { useState, useEffect } from "react";
 import { Button, Menu, MenuItem, Typography, TextField, Box } from "@mui/material";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-import CalendarTodayIcon from '@mui/icons-material/CalendarToday';
+import CalendarTodayIcon from "@mui/icons-material/CalendarToday";
 
 export const VacationSidebarContent: React.FC = () => {
   // 状態変数を初期化します。
   const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null); // メニューのアンカーエレメント
-  const [selectedOption, setSelectedOption] = useState<string>("有給休暇（全休）"); // 選択されたオプション
+  const [selectedOption, setSelectedOption] =
+    useState<string>("有給休暇（全休）"); // 選択されたオプション
   const [time, setTime] = useState<number>(8); // 時間
-  const [headerText, setHeaderText] = useState<string>('休暇申請（全休）'); // // ヘッダーテキスト
+  const [headerText, setHeaderText] = useState<string>("休暇申請（全休）"); // // ヘッダーテキスト
   const [isInputFocused, setIsInputFocused] = useState<boolean>(false); // 入力フィールドにフォーカスがあるかどうかを示すステータス変数です。
   const [selectedDate, setSelectedDate] = useState<Date | null>(null); // 選択された日付状態変数
   const [openDatePicker, setOpenDatePicker] = useState<boolean>(false); //DatePickerアクセスするかどうかを管理する状態変数
@@ -19,15 +20,26 @@ export const VacationSidebarContent: React.FC = () => {
   useEffect(() => {
     if (selectedOption === "有給休暇（全休）") {
       setTime(8); // 時間を8時間に設定
-      setHeaderText('休暇申請（全休）'); // ヘッダーテキスト設定
+      setHeaderText("休暇申請（全休）"); // ヘッダーテキスト設定
     } else if (selectedOption === "有給休暇（半休）") {
       setTime(4); // 時間を4時間に設定
-      setHeaderText('休暇申請（半休）'); // ヘッダーテキスト設定
+      setHeaderText("休暇申請（半休）"); // ヘッダーテキスト設定
     } else if (selectedOption === "本社出勤") {
       setTime(8); // 時間を8時間に設定
-      setHeaderText('本社出社申請'); // ヘッダーテキスト設定
+      setHeaderText("本社出社申請"); // ヘッダーテキスト設定
     }
   }, [selectedOption]);
+
+  // 日付入力値が変更されたときに申請ボタンが有効になっているか確認
+  useEffect(() => {
+    if (dateInputValue.trim() !== "") {
+      // 日付入力値がある場合にボタンを有効
+      setIsSubmitEnabled(true);
+    } else {
+      // 日付入力値が空の場合はボタンを無効
+      setIsSubmitEnabled(false);
+    }
+  }, [dateInputValue]);
 
   // クリックイベントハンドラ
   const handleClick = (event: React.MouseEvent<HTMLDivElement>) => {
@@ -64,26 +76,26 @@ export const VacationSidebarContent: React.FC = () => {
     setOpenDatePicker(false);
   };
 
-    // 今日の日取り計算
-    const today = new Date();
-    today.setHours(0, 0, 0, 0); // 今日の日付の時間を00:00:00に設定
-  
-    // DatePickerで使用する最小日付設定
-    const minDate = today;
-  
-    const handleDateChange = (date: Date | null) => {
-      setSelectedDate(date);
-      if (date) {
-        const formattedDate = date.toLocaleDateString('en-GB'); // 目的の日付形式でフォーマットを指定
-        setDateInputValue(formattedDate); // 入力された値をステータス変数に設定
-      } else {
-        setDateInputValue(""); // 日付がnullの場合、状態変数を初期化
-      }
-    };
+  // 今日の日取り計算
+  const today = new Date();
+  today.setHours(0, 0, 0, 0); // 今日の日付の時間を00:00:00に設定
 
-    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-      setDateInputValue(event.target.value); // 入力された値をステータス変数に設定
-    };
+  // DatePickerで使用する最小日付設定
+  const minDate = today;
+
+  const handleDateChange = (date: Date | null) => {
+    setSelectedDate(date);
+    if (date) {
+      const formattedDate = date.toLocaleDateString("en-GB"); // 目的の日付形式でフォーマットを指定
+      setDateInputValue(formattedDate); // 入力された値をステータス変数に設定
+    } else {
+      setDateInputValue(""); // 日付がnullの場合、状態変数を初期化
+    }
+  };
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setDateInputValue(event.target.value); // 入力された値をステータス変数に設定
+  };
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault(); // 基本提出動作防止
@@ -91,27 +103,41 @@ export const VacationSidebarContent: React.FC = () => {
     // 追加でページ切り替えなどの動作が可能
   };
 
-  const inputBorderStyle = dateInputValue ? 'border-blue-500' : 'border-red-500'; // 日付入力欄のスタイルを条件付きに設定するためのCSSクラス
+  const inputBorderStyle = dateInputValue
+    ? "border-blue-500"
+    : "border-red-500"; // 日付入力欄のスタイルを条件付きに設定するためのCSSクラス
+
+  // 申請ボタンを有効にするかどうかを管理する状態変数
+  const [isSubmitEnabled, setIsSubmitEnabled] = useState<boolean>(false);
 
   return (
-    <div style={{ width: '480px' }}>
+    <div style={{ width: "480px" }}>
       {/* ヘッダーセクション */}
-      <div className={`bg-indigo-500 p-16 mb-16 text-center py-68 ${headerText === '本社出社申請' ? 'bg-gray-900' : ''}`}>
-        <Typography variant="h2" className="text-6xl text-white font-bold">{headerText}</Typography>
+      <div
+        className={`bg-indigo-500 p-16 mb-16 text-center py-68 ${headerText === "本社出社申請" ? "bg-gray-900" : ""}`}
+      >
+        <Typography variant="h2" className="text-6xl text-white font-bold">
+          {headerText}
+        </Typography>
       </div>
 
       {/* 日付 入力 セクション */}
       <div className="p-48 -mt-32">
-        <Typography variant="body1" className="text-base text-black font-bold">日付*</Typography>
-        <div className={`mt-2 flex items-center border ${dateInputValue ? 'border-blue-500' : 'border-red-500'} rounded-md p-8 h-48`} style={{ position: "relative" }}>
+        <Typography variant="body1" className="text-base text-black font-bold">
+          日付*
+        </Typography>
+        <div
+          className={`mt-2 flex items-center border ${dateInputValue ? "border-blue-500" : "border-red-500"} rounded-md p-8 h-48`}
+          style={{ position: "relative" }}
+        >
           {/* カレンダーアイコン */}
           <Box
             sx={{
-              position: 'absolute',
-              left: '12px',
-              top: '50%',
-              transform: 'translateY(-50%)',
-              cursor: 'pointer',
+              position: "absolute",
+              left: "12px",
+              top: "50%",
+              transform: "translateY(-50%)",
+              cursor: "pointer",
               zIndex: 1, // この値を設定して、アイコンが他の要素の上に表示されるようにする
             }}
             onClick={handleDatePickerOpen}
@@ -130,30 +156,41 @@ export const VacationSidebarContent: React.FC = () => {
             className="w-full pl-36" // 入力フィールドの左側の余白を与え、アイコンと重ならないようにする
           />
         </div>
-        <Typography variant="body2" className="text-xs font-bold text-gray-500 mt-4">YYYY-MM-DD</Typography>
+        <Typography
+          variant="body2"
+          className="text-xs font-bold text-gray-500 mt-4"
+        >
+          YYYY-MM-DD
+        </Typography>
       </div>
 
       {/* 区分選択セクション */}
       <div className="p-48 -mt-60">
-        <Typography variant="body1" className="text-base text-black font-bold">区分*</Typography>
+        <Typography variant="body1" className="text-base text-black font-bold">
+          区分*
+        </Typography>
         <div
           className="border border-gray-400 p-8 mt-6 h-48 rounded-md flex items-center" // 枠とスタイリング設定
           id="selectionBox" // 選択ボックスのID
           onClick={handleClick} // クリックするとhandleClick関数呼び出し
           style={{ cursor: "pointer" }} // マウス カーソルをポインタに変更してクリック可能性を示す
         >
-          <Typography variant="body1" className="font-medium pl-6">{selectedOption}</Typography>
+          <Typography variant="body1" className="font-medium pl-6">
+            {selectedOption}
+          </Typography>
         </div>
         {/* オプションメニュー */}
         <Menu
           anchorEl={anchorEl} // メニューが開く目安となるエレメント
           open={Boolean(anchorEl)} // メニューが開いた状態かどうかをBoolean値に設定
           onClose={handleClose} // メニューを閉じるときに呼び出される関数
-          anchorOrigin={{ // メニューの基準位置設定
+          anchorOrigin={{
+            // メニューの基準位置設定
             vertical: "bottom", // 縦方向位置: 下の方
             horizontal: "left", // 横方向位置:左
           }}
-          transformOrigin={{ // メニューが表示される位置設定
+          transformOrigin={{
+            // メニューが表示される位置設定
             vertical: "top", // 縦方向位置: 上
             horizontal: "left", // 横方向位置:左
           }}
@@ -175,13 +212,21 @@ export const VacationSidebarContent: React.FC = () => {
 
       {/* 時間表示セクション */}
       <div className="p-48 -mt-60">
-        <Typography variant="body1" className="text-base text-black font-bold">時間</Typography> {/* 時間 */}
-        <Typography variant="body1" className="ml-6">{time}</Typography> {/* 実時間値出力 */}
+        <Typography variant="body1" className="text-base text-black font-bold">
+          時間
+        </Typography>{" "}
+        {/* 時間 */}
+        <Typography variant="body1" className="ml-6">
+          {time}
+        </Typography>{" "}
+        {/* 実時間値出力 */}
       </div>
 
       {/* 休暇理由入力セクション */}
       <div className="p-48 -mt-60">
-        <Typography variant="body1" className="text-base text-black font-bold">休暇事由 / 備考</Typography>
+        <Typography variant="body1" className="text-base text-black font-bold">
+          休暇事由 / 備考
+        </Typography>
         <textarea
           className="w-full min-h-52 px-4 py-16 text-sm border border-gray-400 rounded"
           placeholder="" // プレースホルダーテキスト
@@ -194,10 +239,11 @@ export const VacationSidebarContent: React.FC = () => {
           type="submit" // ボタンがフォームを提出するように設定
           className="text-base bg-gray-200 text-gray-600 font-bold rounded-full w-full mx-44 -mt-16"
           onClick={handleSubmit}
+          disabled={!isSubmitEnabled}
         >
           申請 {/* 「申請」テキスト */}
         </Button>
       </div>
     </div>
   );
-}
+};
